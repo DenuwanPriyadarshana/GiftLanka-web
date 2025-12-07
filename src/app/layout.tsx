8757917ff4a7672529cc1ce64d41a57daab955d1
DenuwanPriyadarshana <denuwanpriyadarshana8@gmail.com>
2025-12-07 22:14:47 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,18 @@ export default function RootLayout({
     <html lang="en" className="dark">
       {/* Centered Content Layout */}
       <body className={`${inter.className} bg-neutral-950 text-white antialiased flex min-h-screen items-center justify-center`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YKKYTVBNCP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YKKYTVBNCP');
+          `}
+        </Script>
         {children}
       </body>
     </html>
